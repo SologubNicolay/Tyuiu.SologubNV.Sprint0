7b3691c297910b713c6a117e0c16991d91be42b5
Nick <weter905@gmail.com>
2026-09-11 17:24:47 +0500

    Task5

---
+++ Tyuiu.SologubNV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SologubNV.Sprint0.Task5.V0.Lib;
+using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WriteLine("A + B = " + DataService.Addition(1, 5));
+        WriteLine("A - B = " + DataService.Subtraction(15, 5));
+        WriteLine("A * B = " + DataService.Multiplication(10, 10));
+        WriteLine("A / B = " + DataService.Division(9, 3));
+        ReadKey();
+
+    }
+}
