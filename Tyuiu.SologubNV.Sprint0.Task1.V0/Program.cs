2398ef6ffebe1819f32efae0c8562fb3d027a9a4
Nick <weter905@gmail.com>
2026-09-11 14:19:29 +0500

    Task1

---
+++ Tyuiu.SologubNV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Hello, World!");
+string name = "Николай";
+int age = 20;
+Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+Console.ReadKey();
