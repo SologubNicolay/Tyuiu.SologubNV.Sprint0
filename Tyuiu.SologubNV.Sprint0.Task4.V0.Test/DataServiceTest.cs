6c83e7118b23ac8fcfce029aed2f95904d240f62
Nick <weter905@gmail.com>
2026-09-11 17:08:16 +0500

    Task4

---
+++ Tyuiu.SologubNV.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+
+using Tyuiu.SologubNV.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.SologubNV.Sprint0.Task4.V0.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void CheckedAdditionValid()
+        {
+            Assert.Equal(10, DataService.Addition(5, 5));
+        }
+        [Fact]
+        public void CheckedSubtractionValid()
+        {
+            Assert.Equal(5, DataService.Subtraction(10, 5));
+        }
+        [Fact]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.Equal(50, DataService.Multiplication(10, 5));
+        }
+        [Fact]
+        public void CheckedDivisionValid()
+        {
+            Assert.Equal(3, DataService.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.SologubNV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.SologubNV.Sprint0.Task4.V0.Lib;
+using static System.Console;
+
+WriteLine(DataService.Addition(1, 5));
+WriteLine(DataService.Subtraction(15, 5));
+WriteLine(DataService.Multiplication(10, 10));
+WriteLine(DataService.Division(5, 5));
+ReadKey();
