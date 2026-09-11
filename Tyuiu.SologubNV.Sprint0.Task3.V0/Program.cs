596918799bd244078e3a3f78f428ab75cb6c2d30
Nick <weter905@gmail.com>
2026-09-11 16:25:03 +0500

    Task3

---
+++ Tyuiu.SologubNV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.SologubNV.Sprint0.Task3.V0.Lib;
+using static System.Console;
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(DataService.Sum(7, 7));
+            ReadKey();
+        }
+    }
