f183e6c4f760a927edae668bb08aa1260ece97ce
Nick <weter905@gmail.com>
2026-09-11 17:50:33 +0500

    Task6

---
+++ Tyuiu.SologubNV.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+using Tyuiu.SologubNV.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.SologubNV.Sprint0.Task6.V0.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.Equal(15, res);
+        }
+        [Fact]
+        public void ChecksubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.Equal(-15, res);
+        }
+        [Fact]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.Equal(120, res);
+        }
+    }
+}
